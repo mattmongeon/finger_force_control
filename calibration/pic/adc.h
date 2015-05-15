bda151499d91cc3a62c7d7fdde0b03767aa277fb
Matt Mongeon <matthewmongeon2015@u.northwestern.edu>
2015-05-15 16:24:10 -0500

    Getting I2C communication with the ADC to work.  Having problems with receiving data.

--- calibration/pic/adc.h
+++ calibration/pic/adc.h
@@ -5,12 +5,7 @@
 typedef volatile unsigned char* volatile buffer_t;
 
 
-void i2c_setup();
-
-
-// initiate an i2c write read operation at the given address. You can optionally only read or only write by passing zero lengths for the reading or writing
-// this will not return until the transaction is complete.  returns false on error
-int i2c_write_read(unsigned int addr, const buffer_t write, unsigned int wlen, const buffer_t read, unsigned int rlen );
+int init_adc();
 
 
 #endif // INCLUDED_I2C_H
--- calibration/pic/main.c
+++ calibration/pic/main.c
@@ -1,19 +1,48 @@
 #include "NU32.h"  // config bits, constants, funcs for startup and UART
-#include "i2c.h"
+#include "adc.h"
+#include "utils.h"
+
+
+extern volatile int adc_data_ready;
+
 
 int main()
 {
 	NU32_Startup();  // cache on, min flash wait, interrupts on, LED/button init, UART init
+
+	NU32_LED1 = 0;
+	NU32_LED2 = 0;
+	
+	wait_usec(500000);
+
 	NU32_LED1 = 1;
 	NU32_LED2 = 1;
-	__builtin_disable_interrupts();
 
-	i2c_setup();
+
 	
-	__builtin_enable_interrupts();
+	int init_retVal = init_adc();
+	
 
+	int errorLEDs = 0;
+	errorLEDs = (init_retVal != 0);
+	NU32_LED1 = 0;
+	NU32_LED2 = (errorLEDs != 0);
+
+	_CP0_SET_COUNT(0);
+	
 	while(1)
 	{
+		/* if( !NU32_LED2 && adc_data_ready ) */
+		/* 	NU32_LED2 = 1; */
+
+		if( (init_retVal < 0) && (_CP0_GET_COUNT() >= (NU32_SYS_FREQ/8ul)) )
+		{
+			_CP0_SET_COUNT(0);
+			NU32_LED1 = !NU32_LED1;
+			if( errorLEDs )
+				NU32_LED2 = !NU32_LED2;
+		}
+		
 		/* NU32_ReadUART1(buffer, BUF_SIZE); */
 		/* NU32_LED2 = 1; */
 
