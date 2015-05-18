754ea7cd92284878a1b699a21b19aa9e37fb1b04
Matt Mongeon <matthewmongeon2015@u.northwestern.edu>
2015-05-18 13:57:06 -0500

    Code compiles with new files.  Nothing has really been done to make the new code usable or to test it.

--- calibration/pic/motor.h
+++ calibration/pic/motor.h
@@ -4,6 +4,20 @@
 
 #define MAX_CURRENT_MA 449
 
+
+// --- State Enum --- //
+
+typedef enum
+{
+	IDLE = 0,
+	PWM,
+	TUNE,
+	TRACK
+} state_t;  // the various states
+
+
+// --- Function Declarations --- //
+
 void motor_init();  // Initializes the module and the peripherals it uses.
 
 void motor_duty_cycle_pct_set(int duty_pct);
@@ -15,6 +29,10 @@ void motor_mA_set(int mA);  // Specifies the motor current reference in mA.
 void motor_gains_read();  // Read the gains from the client.
 
 void motor_gains_write();  // Sends the motor gains to the client.
+
+state_t motor_state_get();
+
+void motor_state_set(state_t s);
 
 
 #endif // INCLUDED_MOTOR_H
