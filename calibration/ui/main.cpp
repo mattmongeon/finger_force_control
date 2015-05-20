c8188a24b69a8b5d59bbe85d45b2b07803d611a0
Matt Mongeon <matthewmongeon2015@u.northwestern.edu>
2015-05-20 13:03:46 -0500

    Initial commit.  Currently communicates with PIC by sending a command to read a value from the ADC and transmit the raw value to the UI.

---
+++ calibration/ui/main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <string>
+#include "pic_comm.h"
+#include "load_cell_cal.h"
+#include "utils.h"
+
+
+void printValues( double loadCellForce, double biotacForce, int sampleNum )
+{
+	std::cout << nUtils::PREV_LINE << nUtils::PREV_LINE << "\r" << std::flush;
+	std::cout << "Sample " << sampleNum << std::endl;
+	std::cout << "Load Cell Force:  " << loadCellForce << " kg" << std::endl;
+	std::cout << "Biotac Force:  " << biotacForce << " kg" << std::flush;
+}
+
+
+void printMenu()
+{
+	std::cout << "Function Menu" << std::endl;
+	std::cout << "-------------" << std::endl;
+	std::cout << std::endl;
+	std::cout << "c:  Calibrate load cell" << std::endl;
+	std::cout << "q:  Quit" << std::endl;
+	std::cout << std::endl;
+}
+
+
+int main(int argc, char** argv)
+{
+	std::cout << nUtils::CLEAR_CONSOLE << std::flush;
+
+	std::cout << "Opening serial port to communicate with PIC..." << std::endl;
+
+	cPicComm picSerial;
+	if( !picSerial.OpenSerialPort() )
+	{
+		std::cout << "Error opening serial port!" << std::endl;
+		return 0;
+	}
+
+	std::cout << "Serial port opened and ready!" << std::endl;
+	std::cout << std::endl;
+
+
+	// --- Main State Machine --- //
+	
+	bool keepGoing = true;
+	while(keepGoing)
+	{
+		printMenu();
+		switch(nUtils::GetMenuSelection())
+		{
+		case 'c':
+			calibrateLoadCell(picSerial);
+			break;
+
+		case 'q':
+			std::cout << "Exiting..." << std::endl;
+			keepGoing = false;
+			break;
+		}
+
+		std::cout << nUtils::CLEAR_CONSOLE << std::flush;
+	}
+
+	
+	// --- Release Resources --- //
+
+	std::cout << "Releasing resources..." << std::endl;
+	picSerial.CloseSerialPort();
+
+
+	// --- Finished --- //
+
+	std::cout << "Finished.  Goodbye!" << std::endl;
+	
+	return 0;
+}
+
