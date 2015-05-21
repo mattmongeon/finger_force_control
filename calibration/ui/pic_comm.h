0b6ee0f9799f4fa38e52c15c9a9f3888ff38481c
Matt Mongeon <matthewmongeon2015@u.northwestern.edu>
2015-05-21 09:34:59 -0500

    Code cleanup and organization.  Removed some functions that are no longer needed.  Made some functions const.

--- calibration/ui/pic_comm.h
+++ calibration/ui/pic_comm.h
@@ -3,38 +3,88 @@
 
 #include <string>
 #include <termios.h>
+#include <iostream>
 
 
+// This class handles communicating with the PIC32 (NU32) over a serial connection.
 class cPicComm
 {
 public:
 
-	struct sPicData
-	{
-
-	};
-
+	//--------------------------------------------------------------------------//
+	//----------------------  CONSTRUCTION / DESTRUCTION  ----------------------//
+	//--------------------------------------------------------------------------//
 	
-public:
-
 	cPicComm();
 	~cPicComm();
 
+	//--------------------------------------------------------------------------//
+	//--------------------------  INTERFACE FUNCTIONS  -------------------------//
+	//--------------------------------------------------------------------------//
+	
+	// Opens the serial port for communicating with the PIC.  The serial port is
+	// hard-coded to /dev/ttyUSB0 and all of its communication settings are
+	// hard-coded since it is expecting the NU32.
+	//
+	// Return - true if the serial port is successfully opened, false otherwise.
 	bool OpenSerialPort();
+
+	
+	// Closes the serial port used for communicating with the PIC.
 	void CloseSerialPort();
 
-	// Reads the raw ADC value that represents the load cell's output voltage.
-	int ReadRawLoadCellValue();
+	
+	// Template function for reading a particular type of data from the PIC.
+	//
+	// Params:
+	// cmd - the string command used for requesting data from the PIC.  Needs to
+	//       be a single character followed by '\n'.
+	//
+	// Returns - the value requested from the PIC.
+	template<typename T>
+	T ReadValueFromPic(const std::string& cmd) const
+	{
+		WriteCommandToPic(cmd);
 
-	int ReadLoadCellValue_grams();
+		unsigned char buffer[sizeof(T)];
+		ReadFromPic(buffer, sizeof(T));
+
+		T retVal = reinterpret_cast<T&>(buffer);
+		return retVal;
+	}
 	
 	
 private:
 
-	bool WriteCommandToPic(const std::string& cmd);
+	//--------------------------------------------------------------------------//
+	//---------------------------  HELPER FUNCTIONS  ---------------------------//
+	//--------------------------------------------------------------------------//
 
-	bool ReadFromPic(unsigned char* buffer, int numBytes);
+	// Sends the parameter command to the PIC.
+	//
+	// Params:
+	// cmd - the string command used for requesting data from the PIC.  Needs to
+	//       be a single character followed by '\n'.
+	//
+	// Return - true if sent successfully over serial, false otherwise.
+	bool WriteCommandToPic(const std::string& cmd) const;
+
 	
+	// Reads data from the PIC over the serial connection.
+	//
+	// Params:
+	// buffer - the data buffer that will hold the data to be read.
+	// numBytes - the number of bytes to be read from the PIC.
+	//
+	// Return - true if serial communication was successful, false otherwise.
+	bool ReadFromPic(unsigned char* buffer, int numBytes) const;
+
+
+	//--------------------------------------------------------------------------//
+	//-----------------------------  DATA MEMBERS  -----------------------------//
+	//--------------------------------------------------------------------------//
+
+	// The handle used for identifying the serial port.
 	int mPortHandle;
 	
 };
