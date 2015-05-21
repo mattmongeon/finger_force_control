35613235e90ab8e9d5090700d9861f39876aacaf
Matt Mongeon <matthewmongeon2015@u.northwestern.edu>
2015-05-21 11:08:05 -0500

    Initial commit.

---
+++ calibration/pic/load_cell.h
@@ -0,0 +1,13 @@
+#ifndef INCLUDED_LOAD_CELL_H
+#define INCLUDED_LOAD_CELL_H
+
+
+int load_cell_init();
+
+int load_cell_read_grams();
+
+int load_cell_raw_value();
+
+
+#endif // INCLUDED_LOAD_CELL_H
+
