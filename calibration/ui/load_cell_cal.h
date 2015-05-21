fbd3228bb0dc3f5576d0c126d77c6d542f30cfe6
Matt Mongeon <matthewmongeon2015@u.northwestern.edu>
2015-05-21 09:34:11 -0500

    Did some code cleanup and organization.  The cPicComm device is now const.

--- calibration/ui/load_cell_cal.h
+++ calibration/ui/load_cell_cal.h
@@ -5,7 +5,46 @@
 class cPicComm;
 
 
-void calibrateLoadCell(cPicComm& picComm);
+class cLoadCell
+{
+public:
+
+	//--------------------------------------------------------------------------//
+	//----------------------  CONSTRUCTION / DESTRUCTION  ----------------------//
+	//--------------------------------------------------------------------------//
+	
+	cLoadCell(const cPicComm* picComm);
+
+	~cLoadCell();
+
+
+	//--------------------------------------------------------------------------//
+	//--------------------------  INTERFACE FUNCTIONS  -------------------------//
+	//--------------------------------------------------------------------------//
+
+	// Runs through the calibration routine for the load cell.  It will ask the user
+	// for weight values and store a load cell value that has been averaged over
+	// several readings.  When finished, it will print the weights and associated
+	// values to generate a linear fit.
+	void RunCalibrationRoutine();
+
+	// Sends a request to the PIC to read from the load cell and send back the value
+	// in grams.
+	//
+	// Return - the calibrated load cell reading in grams.
+	int ReadLoadCell_grams();
+
+	
+private:
+
+	//--------------------------------------------------------------------------//
+	//-----------------------------  DATA MEMBERS  -----------------------------//
+	//--------------------------------------------------------------------------//
+	
+	// The object used for communication with the PIC.
+	const cPicComm* mpPicComm;
+	
+};
 
 
 #endif  // INCLUDED_CALIBRATION_H
