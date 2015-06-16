ee3ef7ff3ed82dc8ef8c098e9959f1ef6f6fabce
Matt Mongeon <matthewmongeon2015@u.northwestern.edu>
2015-06-16 12:40:46 -0500

    Added a menu option for continuously reading from the BioTac.

--- calibration/ui/main.cpp
+++ calibration/ui/main.cpp
@@ -3,6 +3,22 @@
 #include "pic_serial.h"
 #include "load_cell.h"
 #include "utils.h"
+#include "stopwatch.h"
+#include <pthread.h>
+
+
+static bool stopLoop = false;
+
+
+void* keyboardThread(void* pIn)
+{
+	// Just wait for the user to press ENTER.
+	std::cin.ignore();
+
+	stopLoop = true;
+
+	return NULL;
+}
 
 
 void printValues( double loadCellForce, double biotacForce, int sampleNum )
@@ -23,6 +39,7 @@ void printMenu()
 	std::cout << "c:  Calibrate load cell" << std::endl;
 	std::cout << "l:  Read load cell" << std::endl;
 	std::cout << "p:  Set motor PWM duty cycle" << std::endl;
+	std::cout << "r:  Continuously read from BioTac" << std::endl;
 	std::cout << "q:  Quit" << std::endl;
 	std::cout << std::endl;
 }
@@ -61,6 +78,9 @@ struct sBioTacData
 int main(int argc, char** argv)
 {
 	std::cout << nUtils::CLEAR_CONSOLE << std::flush;
+
+
+	// --- Serial Communication --- //
 	
 	std::cout << "Opening serial port to communicate with PIC..." << std::endl;
 
@@ -74,13 +94,23 @@ int main(int argc, char** argv)
 	std::cout << "Serial port opened and ready!" << std::endl;
 	std::cout << std::endl;
 
+
+	// --- Devices --- //
+	
 	std::cout << "Constructing load cell instance" << std::endl;
 	cLoadCell loadCell(&picSerial);
 
-	std::cout << "Ready to begin!" << std::endl;
 
+	// --- Miscellaneous --- //
+	
+	std::cout << "Setting up thread for keyboard input" << std::endl;
+
+	pthread_t keyboardThreadHandle;
+	
 
 	// --- Main State Machine --- //
+
+	std::cout << "Ready to begin!" << std::endl;
 	
 	bool keepGoing = true;
 	while(keepGoing)
@@ -90,7 +120,7 @@ int main(int argc, char** argv)
 		{
 		case 'b':
 		{
-			std::cout << "Read from BioTac (e1):  " << std::flush;
+			std::cout << "Read from BioTac (e1):" << std::endl;
 			sBioTacData d = picSerial.ReadValueFromPic<sBioTacData>(nUtils::READ_BIOTAC);
 			std::cout << "E1:  " << d.e1 << std::endl;
 			std::cout << "PAC:  " << d.pac << std::endl;
@@ -120,6 +150,55 @@ int main(int argc, char** argv)
 			std::cin >> pwm;
 			picSerial.WriteValueToPic<char>(nUtils::SET_PWM, static_cast<char>(pwm));
 			std::cout << "Sent!" << std::endl;
+			break;
+		}
+
+		case 'r':
+		{
+			cStopwatch stopwatch;
+			stopLoop = false;
+			pthread_create(&keyboardThreadHandle, NULL, keyboardThread, NULL);
+			stopwatch.Start();
+			while(!stopLoop)
+			{
+				std::cout << nUtils::CLEAR_CONSOLE << std::flush;
+				std::cout << "Read continuously from BioTac" << std::endl;
+				std::cout << "Press ENTER to quit" << std::endl;
+				std::cout << std::endl;
+				sBioTacData d = picSerial.ReadValueFromPic<sBioTacData>(nUtils::READ_BIOTAC);
+				std::cout << "E1:  " << d.e1 << "\r\n";
+				std::cout << "E2:  " << d.e2 << "\r\n";
+				std::cout << "E3:  " << d.e3 << "\r\n";
+				std::cout << "E4:  " << d.e4 << "\r\n";
+				std::cout << "E5:  " << d.e5 << "\r\n";
+				std::cout << "E6:  " << d.e6 << "\r\n";
+				std::cout << "E7:  " << d.e7 << "\r\n";
+				std::cout << "E8:  " << d.e8 << "\r\n";
+				std::cout << "E9:  " << d.e9 << "\r\n";
+				std::cout << "E10:  " << d.e10 << "\r\n";
+				std::cout << "E11:  " << d.e11 << "\r\n";
+				std::cout << "E12:  " << d.e12 << "\r\n";
+				std::cout << "E13:  " << d.e13 << "\r\n";
+				std::cout << "E14:  " << d.e14 << "\r\n";
+				std::cout << "E15:  " << d.e15 << "\r\n";
+				std::cout << "E16:  " << d.e16 << "\r\n";
+				std::cout << "E17:  " << d.e17 << "\r\n";
+				std::cout << "E18:  " << d.e18 << "\r\n";
+				std::cout << "E19:  " << d.e19 << "\r\n";
+				std::cout << "PAC:  " << d.pac << "\r\n";
+				std::cout << "PDC:  " << d.pdc << "\r\n";
+				std::cout << "TAC:  " << d.tac << "\r\n";
+				std::cout << "TDC:  " << d.tdc << "\r\n";
+				std::cout << std::endl;
+				stopwatch.Reset();
+				while(stopwatch.GetElapsedTime_ms() < 100.0)
+				{
+					;
+				}
+			}
+			stopwatch.Stop();
+			stopLoop = false;
+			pthread_join(keyboardThreadHandle, NULL);
 			break;
 		}
 
