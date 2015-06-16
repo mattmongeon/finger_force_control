0bb6d66059ad9dd7423054dd223a0997c3725e45
Matt Mongeon <matthewmongeon2015@u.northwestern.edu>
2015-06-16 12:39:25 -0500

    Added a timing function.

--- calibration/ui/utils.cpp
+++ calibration/ui/utils.cpp
@@ -1,6 +1,7 @@
 #include "utils.h"
 #include <iostream>
 #include <string>
+#include <time.h>
 
 
 char nUtils::GetMenuSelection()
@@ -9,4 +10,13 @@ char nUtils::GetMenuSelection()
 	std::cout << "Selection:  " << std::flush;
 	std::cin >> selection;
 	return selection[0];
+}
+
+double nUtils::GetSysTime_ms()
+{
+	struct timespec ts;
+	clock_gettime( CLOCK_REALTIME, &ts );
+	double time_ms = static_cast<double>(ts.tv_nsec) / 1000000.0;
+	time_ms += static_cast<double>(ts.tv_sec) * 1000.0;
+	return time_ms;
 }
