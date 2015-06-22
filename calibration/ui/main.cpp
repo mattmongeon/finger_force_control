87f6d73bac24e6ac2310a8363fc5f8fc9d0e3240
Matt Mongeon <matthewmongeon2015@u.northwestern.edu>
2015-06-22 14:17:15 -0500

    Added a few new menu options, namely being able to tune the force tracking.

--- calibration/ui/main.cpp
+++ calibration/ui/main.cpp
@@ -7,15 +7,27 @@
 #include <pthread.h>
 
 
+static bool detectEnterPress = false;
 static bool stopLoop = false;
+static bool stopThread = false;
 
 
 void* keyboardThread(void* pIn)
 {
-	// Just wait for the user to press ENTER.
-	std::cin.ignore();
-
-	stopLoop = true;
+	while(!stopThread)
+	{
+		if( detectEnterPress )
+		{
+			// Just wait for the user to press q+ENTER.
+			std::string input;
+			std::cin >> input;
+			if( input == "q" )
+			{
+				stopLoop = true;
+				detectEnterPress = false;
+			}
+		}
+	}
 
 	return NULL;
 }
@@ -37,9 +49,14 @@ void printMenu()
 	std::cout << std::endl;
 	std::cout << "b:  Single read from BioTac" << std::endl;
 	std::cout << "c:  Calibrate load cell" << std::endl;
+	std::cout << "f:  Hold force" << std::endl;
+	std::cout << "g:  Set torque controller gains" << std::endl;
+	std::cout << "h:  Get torque controller gains" << std::endl;
 	std::cout << "l:  Read load cell" << std::endl;
 	std::cout << "p:  Set motor PWM duty cycle" << std::endl;
 	std::cout << "r:  Continuously read from BioTac" << std::endl;
+	std::cout << "s:  Continuously read from load cell" << std::endl;
+	std::cout << "t:  Tune torque gains" << std::endl;
 	std::cout << "q:  Quit" << std::endl;
 	std::cout << std::endl;
 }
@@ -74,6 +91,14 @@ struct sBioTacData
 	unsigned short tdc;
 };
 
+struct sTorqueTuneData
+{
+	int mLoadCell_g;
+	int mError;
+	int mErrorInt;
+	unsigned int mTimeStamp;
+};
+
 
 int main(int argc, char** argv)
 {
@@ -106,12 +131,16 @@ int main(int argc, char** argv)
 	std::cout << "Setting up thread for keyboard input" << std::endl;
 
 	pthread_t keyboardThreadHandle;
+	pthread_create(&keyboardThreadHandle, NULL, keyboardThread, NULL);
 	
 
 	// --- Main State Machine --- //
 
 	std::cout << "Ready to begin!" << std::endl;
-	
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+
 	bool keepGoing = true;
 	while(keepGoing)
 	{
@@ -122,11 +151,29 @@ int main(int argc, char** argv)
 		{
 			std::cout << "Read from BioTac (e1):" << std::endl;
 			sBioTacData d = picSerial.ReadValueFromPic<sBioTacData>(nUtils::READ_BIOTAC);
-			std::cout << "E1:  " << d.e1 << std::endl;
-			std::cout << "PAC:  " << d.pac << std::endl;
-			std::cout << "PDC:  " << d.pdc << std::endl;
-			std::cout << "TAC:  " << d.tac << std::endl;
-			std::cout << "TDC:  " << d.tdc << std::endl;
+			std::cout << "E1:  " << d.e1 << "\r\n";
+			std::cout << "E2:  " << d.e2 << "\r\n";
+			std::cout << "E3:  " << d.e3 << "\r\n";
+			std::cout << "E4:  " << d.e4 << "\r\n";
+			std::cout << "E5:  " << d.e5 << "\r\n";
+			std::cout << "E6:  " << d.e6 << "\r\n";
+			std::cout << "E7:  " << d.e7 << "\r\n";
+			std::cout << "E8:  " << d.e8 << "\r\n";
+			std::cout << "E9:  " << d.e9 << "\r\n";
+			std::cout << "E10:  " << d.e10 << "\r\n";
+			std::cout << "E11:  " << d.e11 << "\r\n";
+			std::cout << "E12:  " << d.e12 << "\r\n";
+			std::cout << "E13:  " << d.e13 << "\r\n";
+			std::cout << "E14:  " << d.e14 << "\r\n";
+			std::cout << "E15:  " << d.e15 << "\r\n";
+			std::cout << "E16:  " << d.e16 << "\r\n";
+			std::cout << "E17:  " << d.e17 << "\r\n";
+			std::cout << "E18:  " << d.e18 << "\r\n";
+			std::cout << "E19:  " << d.e19 << "\r\n";
+			std::cout << "PAC:  " << d.pac << "\r\n";
+			std::cout << "PDC:  " << d.pdc << "\r\n";
+			std::cout << "TAC:  " << d.tac << "\r\n";
+			std::cout << "TDC:  " << d.tdc << "\r\n";
 			std::cout << std::endl;
 			break;
 		}
@@ -134,6 +181,52 @@ int main(int argc, char** argv)
 		case 'c':
 		{
 			loadCell.RunCalibrationRoutine();
+			break;
+		}
+
+		case 'f':
+		{
+			picSerial.WriteCommandToPic(nUtils::HOLD_FORCE);
+
+			std::cout << "Enter force to hold (g):  ";
+
+			int force;
+			std::cin >> force;
+
+			picSerial.WriteToPic( reinterpret_cast<unsigned char*>(&force), sizeof(int) );
+			break;
+		}
+
+		case 'g':
+		{
+			float k[3];
+			
+			std::cout << "Enter Kp:  ";
+			std::cin >> k[0];
+
+			std::cout << "Enter Ki:  ";
+			std::cin >> k[1];
+
+			std::cout << "Enter Kd:  ";
+			std::cin >> k[2];
+
+			picSerial.WriteCommandToPic(nUtils::SET_TORQUE_CTRL_GAINS);
+			picSerial.WriteToPic(reinterpret_cast<unsigned char*>(&k), sizeof(k));
+		}
+
+		case 'h':
+		{
+			picSerial.WriteCommandToPic(nUtils::GET_TORQUE_CTRL_GAINS);
+			
+			unsigned char buffer[20];
+			picSerial.ReadFromPic(buffer, 12);
+
+			float* pF = reinterpret_cast<float*>(&buffer);
+			std::cout << "Kp:  " << pF[0] << std::endl;
+			std::cout << "Ki:  " << pF[1] << std::endl;
+			std::cout << "kd:  " << pF[2] << std::endl;
+			std::cout << std::endl;
+			
 			break;
 		}
 
@@ -157,14 +250,15 @@ int main(int argc, char** argv)
 		{
 			cStopwatch stopwatch;
 			stopLoop = false;
-			pthread_create(&keyboardThreadHandle, NULL, keyboardThread, NULL);
+			detectEnterPress = true;
 			stopwatch.Start();
+			
 			while(!stopLoop)
 			{
 				std::cout << nUtils::CLEAR_CONSOLE << std::flush;
-				std::cout << "Read continuously from BioTac" << std::endl;
-				std::cout << "Press ENTER to quit" << std::endl;
-				std::cout << std::endl;
+				std::cout << "Read continuously from BioTac\r\n";
+				std::cout << "Enter q + ENTER to quit\r\n";
+				std::cout << "\r\n";
 				sBioTacData d = picSerial.ReadValueFromPic<sBioTacData>(nUtils::READ_BIOTAC);
 				std::cout << "E1:  " << d.e1 << "\r\n";
 				std::cout << "E2:  " << d.e2 << "\r\n";
@@ -190,15 +284,68 @@ int main(int argc, char** argv)
 				std::cout << "TAC:  " << d.tac << "\r\n";
 				std::cout << "TDC:  " << d.tdc << "\r\n";
 				std::cout << std::endl;
+
 				stopwatch.Reset();
 				while(stopwatch.GetElapsedTime_ms() < 100.0)
 				{
 					;
 				}
 			}
-			stopwatch.Stop();
+
+			break;
+		}
+
+		case 's':
+		{
+			cStopwatch stopwatch;
 			stopLoop = false;
-			pthread_join(keyboardThreadHandle, NULL);
+			detectEnterPress = true;
+			stopwatch.Start();
+
+			while(!stopLoop)
+			{
+				std::cout << nUtils::CLEAR_CONSOLE << std::flush;
+				std::cout << "Read continuously from load cell\r\n";
+				std::cout << "Enter q+ENTER to quit\r\n";
+				std::cout << "\r\n";
+				std::cout << "Force:  " << loadCell.ReadLoadCell_grams() << " g\r\n";
+				std::cout << std::endl;
+
+				stopwatch.Reset();
+				while(stopwatch.GetElapsedTime_ms() < 100.0)
+				{
+					;
+				}
+			}
+
+			break;
+		}
+
+		case 't':
+		{
+			picSerial.WriteCommandToPic(nUtils::TUNE_TORQUE_GAINS);
+
+			std::cout << "Enter force to hold (g):  ";
+
+			int force;
+			std::cin >> force;
+
+			picSerial.WriteToPic( reinterpret_cast<unsigned char*>(&force), sizeof(int) );
+
+			std::cout << "Waiting for tuning results..." << std::endl;
+
+			sTorqueTuneData tuneData[100];
+			picSerial.ReadFromPic( reinterpret_cast<unsigned char*>(&tuneData), sizeof(sTorqueTuneData)*100 );
+
+			std::cout << "Tuning results received!" << std::endl;
+			std::cout << "Load Cell\t\tError\t\tError Integral\t\tTimestamp" << std::endl;
+			for( int i = 0; i < 100; ++i )
+			{
+				std::cout << tuneData[i].mLoadCell_g << "\t\t" << tuneData[i].mError << "\t\t" << tuneData[i].mErrorInt << "\t\t" << tuneData[i].mTimeStamp << std::endl;
+			}
+
+			std::cout << std::endl;
+			
 			break;
 		}
 
@@ -216,6 +363,9 @@ int main(int argc, char** argv)
 
 	std::cout << "Releasing resources..." << std::endl;
 	picSerial.CloseSerialPort();
+
+	stopThread = true;
+	pthread_join(keyboardThreadHandle, NULL);
 
 
 	// --- Finished --- //
