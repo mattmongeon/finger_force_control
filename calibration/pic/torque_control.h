0268fa5d2c1d7be4839ed3583c73f95628d90284
Matt Mongeon <matthewmongeon2015@u.northwestern.edu>
2015-06-22 14:15:38 -0500

    Initial commit.

---
+++ calibration/pic/torque_control.h
@@ -0,0 +1,35 @@
+#ifndef INCLUDED_TORQUE_CONTROL_H
+#define INCLUDED_TORQUE_CONTROL_H
+
+
+// This file contains function declarations for the module that is responsible for
+// trying to generate a requested motor torque value based on the desired force
+// at the contact point.  It contains the PID controller for turning the error in
+// desired end-point force into a desired torque value from the motor.  The motor
+// controller will be responsible for using the torque error to generate a PWM
+// value for changing the current through the motor.
+
+
+// --- Structs --- //
+
+typedef struct
+{
+	int load_cell_g;
+	int error;
+	int error_int;
+	unsigned int timestamp;
+} torque_tune_data;
+
+
+// --- Function Declarations --- //
+
+void torque_control_init();
+
+void torque_control_set_desired_force(int force_g);
+
+void torque_control_set_gains(float kp_new, float ki_new, float kd_new);
+
+unsigned char* torque_control_get_raw_tune_buffer();
+
+#endif  // INCLUDED_TORQUE_CONTROL_H
+
