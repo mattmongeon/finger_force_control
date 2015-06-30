d04a92d7a84a14ab3975854fc0074f7e2ffb5e72
Matt Mongeon <matthewmongeon2015@u.northwestern.edu>
2015-06-30 15:07:22 -0500

    Gain tuning has been moved to a function in this class.

--- calibration/ui/load_cell.cpp
+++ calibration/ui/load_cell.cpp
@@ -1,6 +1,7 @@
 #include "load_cell.h"
 #include "utils.h"
 #include "pic_serial.h"
+#include <plplot/plplot.h>
 #include <vector>
 #include <utility>
 #include <iostream>
@@ -93,5 +94,65 @@ void cLoadCell::RunCalibrationRoutine()
 int cLoadCell::ReadLoadCell_grams()
 {
 	return mpPicSerial->ReadValueFromPic<int>(nUtils::READ_LOAD_CELL);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void cLoadCell::TuneForceHolding()
+{
+	mpPicSerial->WriteCommandToPic(nUtils::TUNE_TORQUE_GAINS);
+
+	std::cout << "Enter force to hold (g):  ";
+
+	int force;
+	std::cin >> force;
+
+	mpPicSerial->WriteToPic( reinterpret_cast<unsigned char*>(&force), sizeof(int) );
+
+	std::cout << "Waiting for tuning results..." << std::endl;
+
+	sTorqueTuneData tuneData[200];
+	mpPicSerial->ReadFromPic( reinterpret_cast<unsigned char*>(&tuneData), sizeof(sTorqueTuneData)*200 );
+
+	std::cout << "Tuning results received!" << std::endl;
+	std::cout << "Load Cell\tError\t\tError Integral\t\tCurrent (mA)\t\tTimestamp\t\tExe Time (ms)" << std::endl;
+	for( int i = 0; i < 200; ++i )
+	{
+		std::cout << tuneData[i].mLoadCell_g << "\t\t" << tuneData[i].mError << "\t\t" << tuneData[i].mErrorInt << "\t\t" << tuneData[i].mCurrent_mA << "\t\t" << tuneData[i].mTimeStamp << "\t\t" << tuneData[i].mLoopExeTime_ms << std::endl;
+	}
+
+	std::cout << std::endl;
+
+
+	// --- Plot Results --- //
+
+	std::vector<PLFLT> x;
+	std::vector<PLFLT> y;
+	PLFLT yMax = -1000000.0;
+	PLFLT yMin = 1000000.0;
+
+	for( int i = 0; i < 200; ++i )
+	{
+		x.push_back(i);
+
+		yMax = std::max<PLFLT>(yMax, tuneData[i].mLoadCell_g);
+		yMin = std::min<PLFLT>(yMin, tuneData[i].mLoadCell_g);
+		y.push_back(tuneData[i].mLoadCell_g);
+	}
+
+	plsdev("xcairo");
+	plscolbg(255, 255, 255);
+	plinit();
+
+	plscolbg(0,0,0);  // The first call to plscolbg() edits the value stored for color index 0.  Set it back to black.
+	plcol0(nUtils::enumPLplotColor_BLACK);
+	plenv(0, 200, yMin, yMax, 0, 0);
+	pllab("Measurement", "Force (g)", "Trial results");
+
+	plcol0(nUtils::enumPLplotColor_BLUE);
+	plline(200, &(x[0]), &(y[0]));
+
+	plend();
+
 }
 
