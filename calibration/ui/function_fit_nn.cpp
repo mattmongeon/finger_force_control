1dc8b8ef9214cf318138828f98af01bf04f8020c
Matt Mongeon <matthewmongeon2015@u.northwestern.edu>
2015-07-20 10:56:10 -0500

    Now training the neural network using PAC, PDC, TAC, TDC.  Also normalized the values (oops, I forgot about needing to do that).

--- calibration/ui/function_fit_nn.cpp
+++ calibration/ui/function_fit_nn.cpp
@@ -1,7 +1,12 @@
 #include "function_fit_nn.h"
 #include "data_file_reader.h"
+#include "utils.h"
 #include "../common/biotac_comm.h"
 #include <fstream>
+#include <iostream>
+
+
+#define MAX_BIOTAC_LOAD_G 5098.581064889641  // 50 N
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -9,12 +14,12 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 cFunctionFitNN::cFunctionFitNN()
-	: mNumLayers(3),
-	  mNumInputs(23),
+	: mNumLayers(4),
+	  mNumInputs(4),
 	  mNumHiddenNeurons(25),
 	  mNumOutputs(1)
 {
-	mpANN = fann_create_standard(mNumLayers, mNumInputs, mNumHiddenNeurons, mNumOutputs);
+	mpANN = fann_create_standard(mNumLayers, mNumInputs, mNumHiddenNeurons, mNumHiddenNeurons, mNumOutputs);
 
 	fann_set_activation_function_hidden(mpANN, FANN_SIGMOID_SYMMETRIC);
 	fann_set_activation_function_output(mpANN, FANN_SIGMOID_SYMMETRIC);
@@ -41,16 +46,45 @@ void cFunctionFitNN::LoadNeuralNetwork(const std::string& filePath)
 
 void cFunctionFitNN::TrainAgainstDataFiles(const std::vector<std::string>& files)
 {
+	nUtils::ClearConsole();
+	std::cout << "Training on " << files.size() << " total files." << std::endl;
+
 	float desiredError = 0.001;
-	unsigned int maxEpochs = 5000;
+	unsigned int maxEpochs = 10000;
 	unsigned int epochsBetweenReports = 1000;
 	for( std::size_t i = 0; i < files.size(); ++i )
 	{
-		fann_train_on_file(mpANN,
-						   ConvertDataFileToNNFile(files[i]).c_str(),
-						   maxEpochs,
-						   epochsBetweenReports,
-						   desiredError);
+		std::cout << std::endl;
+		std::cout << "Training on " << files[i] << std::endl;
+		std::cout << std::endl;
+
+		fann_train_data* pTrainData = fann_read_train_from_file( ConvertDataFileToNNFile(files[i]).c_str() );
+		fann_set_training_algorithm(mpANN, FANN_TRAIN_INCREMENTAL);
+		// fann_set_activation_steepness_hidden(mpANN, 1);
+		// fann_set_activation_steepness_hidden(mpANN, 1);
+
+		fann_set_train_stop_function(mpANN, FANN_STOPFUNC_BIT);
+		fann_set_bit_fail_limit(mpANN, 0.01f);
+		
+		fann_set_training_algorithm(mpANN, FANN_TRAIN_RPROP);
+
+		fann_init_weights(mpANN, pTrainData);
+
+		fann_train_on_data(mpANN, pTrainData, maxEpochs, epochsBetweenReports, desiredError);
+
+		std::cout << "Inputs:  PDC, PAC, TDC, TAC" << std::endl;
+		for(unsigned int j = 0; j < fann_length_train_data(pTrainData); j++)
+		{
+			fann_type* calc_out = fann_run(mpANN, pTrainData->input[j]);
+			std::cout << "("
+					  << pTrainData->input[j][0] << ", "
+					  << pTrainData->input[j][1] << ", "
+					  << pTrainData->input[j][2] << ", "
+					  << pTrainData->input[j][3] << ")"
+					  << " -> " << calc_out[0] * MAX_BIOTAC_LOAD_G << ", should be " << pTrainData->output[j][0] * MAX_BIOTAC_LOAD_G
+					  << ", difference=" << fann_abs(calc_out[0] - pTrainData->output[j][0]) * MAX_BIOTAC_LOAD_G << "\r\n";
+		}
+		std::cout << std::flush;
 	}
 }
 
@@ -80,35 +114,17 @@ std::string cFunctionFitNN::ConvertDataFileToNNFile(const std::string& filePath)
 
 	std::vector<biotac_tune_data> data = df.GetData();
 	outFile << data.size() << " " << mNumInputs << " " << mNumOutputs << std::endl;
-		
+
+	outFile.precision(20);
 	for( std::size_t i = 0; i < data.size(); ++i )
 	{
-		outFile << static_cast<float>(data[i].mData.pac) << " "
-				<< static_cast<float>(data[i].mData.pdc) << " "
-				<< static_cast<float>(data[i].mData.tac) << " "
-				<< static_cast<float>(data[i].mData.tdc) << " "
-				<< static_cast<float>(data[i].mData.e1) << " "
-				<< static_cast<float>(data[i].mData.e2) << " "
-				<< static_cast<float>(data[i].mData.e3) << " "
-				<< static_cast<float>(data[i].mData.e4) << " "
-				<< static_cast<float>(data[i].mData.e5) << " "
-				<< static_cast<float>(data[i].mData.e6) << " "
-				<< static_cast<float>(data[i].mData.e7) << " "
-				<< static_cast<float>(data[i].mData.e8) << " "
-				<< static_cast<float>(data[i].mData.e9) << " "
-				<< static_cast<float>(data[i].mData.e10) << " "
-				<< static_cast<float>(data[i].mData.e11) << " "
-				<< static_cast<float>(data[i].mData.e12) << " "
-				<< static_cast<float>(data[i].mData.e13) << " "
-				<< static_cast<float>(data[i].mData.e14) << " "
-				<< static_cast<float>(data[i].mData.e15) << " "
-				<< static_cast<float>(data[i].mData.e16) << " "
-				<< static_cast<float>(data[i].mData.e17) << " "
-				<< static_cast<float>(data[i].mData.e18) << " "
-				<< static_cast<float>(data[i].mData.e19) << " "
+		outFile << static_cast<float>(data[i].mData.pdc / 4096.0) << " "
+				<< static_cast<float>(data[i].mData.pac / 4096.0) << " "
+				<< static_cast<float>(data[i].mData.tdc / 4096.0) << " "
+				<< static_cast<float>(data[i].mData.tac / 4096.0) << " "
 				<< std::endl;
 
-		outFile << static_cast<float>(data[i].mLoadCell_g) << std::endl;
+		outFile << static_cast<float>(data[i].mLoadCell_g / MAX_BIOTAC_LOAD_G) << std::endl;
 	}
 
 	outFile.close();
