5bbb82b794adbd3e2fc888bf9991cb5994aee931
Matt Mongeon <matthewmongeon2015@u.northwestern.edu>
2015-07-23 11:25:00 -0500

    Added some protection for the BioTac by limiting the PWM in various scenarios so that it doesn't get slammed into the load cell.

--- calibration/pic/motor.h
+++ calibration/pic/motor.h
@@ -10,6 +10,12 @@ void motor_duty_cycle_pct_set(int duty_pct);
 
 void motor_pwm_set(int pwm_new);  // Specifies the user's PWM value.
 
+// Returns the current PWM value used by the motor.  Will be a signed value
+// based on the direction of the motor.
+//
+// Return - A signed integer specifying the current PWM value.
+int motor_pwm_get();
+
 
 #endif // INCLUDED_MOTOR_H
 
