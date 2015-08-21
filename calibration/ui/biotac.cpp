d821b6140ba73c775af659d796bc95ba6deb3c55
Matt Mongeon <matthewmongeon2015@u.northwestern.edu>
2015-08-21 16:23:45 -0500

    Cleaned up what files are used for testing and which ones for training. Outputting some new CSV files for analyzing errors.  Modified menu wording slightly.

--- calibration/ui/biotac.cpp
+++ calibration/ui/biotac.cpp
@@ -9,8 +9,10 @@
 #include "electrode_tdc_compensator.h"
 #include "biotac_force_curve.h"
 #include "file_utils.h"
+#include "data_file_reader.h"
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -44,8 +46,8 @@ void cBioTac::DisplayMenu()
 		std::cout << "BioTac Function Menu" << std::endl;
 		std::cout << "--------------------" << std::endl;
 		std::cout << std::endl;
-		std::cout << "a: Record force hold for time" << std::endl;
-		std::cout << "b: Single read" << std::endl;
+		std::cout << "a: Record load cell force hold for time" << std::endl;
+		std::cout << "b: Single data read" << std::endl;
 		std::cout << "c: Test BioTac force sensing" << std::endl;
 		std::cout << "d: Continuously read from BioTac" << std::endl;
 		std::cout << "e: Calibrate BioTac with trajectory" << std::endl;
@@ -323,7 +325,7 @@ void cBioTac::RecordForceHold()
 	mpPicSerial->DiscardIncomingData(0);
 	mpPicSerial->WriteCommandToPic(nUtils::BIOTAC_CAL_SINGLE);
 
-	std::cout << "Enter number of seconds for this run (s):  " ;
+	std::cout << "Enter number of seconds to hold force (s):  " ;
 	int seconds = 0;
 	std::cin >> seconds;
 	mpPicSerial->WriteValueToPic(seconds);
@@ -333,20 +335,26 @@ void cBioTac::RecordForceHold()
 	int force;
 	std::cin >> force;
 
-	cRealTimePlot plotter("Load Cell", "Sample", "Force (g)", "Force (g)", "", "", "", seconds * 100.0);
+	cRealTimePlot plotter("Load Cell", "Sample", "Force (g)", "Force (g)", "", "", "", (seconds+1) * 100.0);
 
 	// Now start the process.
 	mpPicSerial->WriteToPic( reinterpret_cast<unsigned char*>(&force), sizeof(int) );
 
 	std::cout << "Waiting for tuning results..." << std::endl;
 
+	int numDataSamples = 0;
 	while(true)
 	{
 		mpPicSerial->ReadFromPic( reinterpret_cast<unsigned char*>(&rxData), sizeof(biotac_tune_data) );
 
 		if( memcmp(&rxData, &stopCondition, sizeof(biotac_tune_data)) != 0 )
 		{
-			logger.LogDataBuffer(&rxData, sizeof(biotac_tune_data));
+			// Ignore the first 100 samples (1 second) to allow all transients to go away.
+			if( numDataSamples >= 100 )
+				logger.LogDataBuffer(&rxData, sizeof(biotac_tune_data));
+
+			++numDataSamples;
+			
 			plotter.AddDataPoint(rxData.mLoadCell_g);
 			tuneData.push_back(rxData);
 		}
@@ -517,43 +525,16 @@ void cBioTac::TrainElectrodeCompenstors()
 	// --- Train For Fit --- //
 			
 	cFunctionFitNLS fit;
-	std::vector<std::string> files;
-	files.push_back("./data/tdc_electrodes/cold_start.dat");
-	// files.push_back("./data/tdc_electrodes/cooling_down_from_high.dat");
-	// files.push_back("./data/tdc_electrodes/data_2015_07_30_09_44_43.dat");
-	files.push_back("./data/tdc_electrodes/data_2015_08_04_15_27_33.dat");
-	files.push_back("./data/tdc_electrodes/data_2015_08_04_16_15_04.dat");
-	// files.push_back("./data/tdc_electrodes/heating_up.dat");
-	// files.push_back("./data/tdc_electrodes/high_temp.dat");
-	// files.push_back("./data/tdc_electrodes/zero1.dat");
-	// files.push_back("./data/tdc_electrodes/zero2.dat");
-	// files.push_back("./data/tdc_electrodes/zero3.dat");
-	files.push_back("./data/tdc_electrodes/unforced_already_warm.dat");
-	files.push_back("./data/tdc_electrodes/cold_soak_to_hot.dat");
-	files.push_back("./data/tdc_electrodes/full_heat_up.dat");
-	files.push_back("./data/tdc_electrodes/reheat_after_heat_and_cooldown.dat");
+	std::vector<std::string> files = nFileUtils::GetFilesInDirectory("./data/tdc_electrodes", ".dat");
+	for( std::size_t i = 0; i < files.size(); ++i )
+		files[i].insert(0, "./data/tdc_electrodes/");
 
 	std::vector<cElectrodeTdcCompensator> compensators = fit.TrainAgainstDataFiles(files);
 
 
 	// --- Test The Fit --- //
 			
-	files.clear();
-	files.push_back("./data/tdc_electrodes/cold_start.dat");
-	files.push_back("./data/tdc_electrodes/cooling_down_from_high.dat");
-	files.push_back("./data/tdc_electrodes/data_2015_07_30_09_44_43.dat");
-	files.push_back("./data/tdc_electrodes/data_2015_08_04_15_27_33.dat");
-	files.push_back("./data/tdc_electrodes/data_2015_08_04_16_15_04.dat");
-	files.push_back("./data/tdc_electrodes/heating_up.dat");
-	files.push_back("./data/tdc_electrodes/high_temp.dat");
-	files.push_back("./data/tdc_electrodes/zero1.dat");
-	files.push_back("./data/tdc_electrodes/zero2.dat");
-	files.push_back("./data/tdc_electrodes/zero3.dat");
-	files.push_back("./data/tdc_electrodes/unforced_already_warm.dat");
-	files.push_back("./data/tdc_electrodes/cold_soak_to_hot.dat");
-	files.push_back("./data/tdc_electrodes/full_heat_up.dat");
-	files.push_back("./data/tdc_electrodes/reheat_after_heat_and_cooldown.dat");
-	fit.TestAgainstDataFiles(files, compensators);
+	// fit.TestAgainstDataFiles(files, compensators);
 
 
 	// --- Write Coefficients To File --- //
@@ -583,41 +564,98 @@ void cBioTac::TrainForceVectorCoefficients()
 	// --- Train Fit For Force Equation --- //
 			
 	cFunctionFitForceTerms ffTerms(compensators);
-	std::vector<std::string> files = nFileUtils::GetFilesInDirectory("./data", ".dat");
+	std::vector<std::string> files = nFileUtils::GetFilesInDirectory("./data/force", ".dat");
 	for( std::size_t i = 0; i < files.size(); ++i )
-		files[i].insert(0, "./data/");
+		files[i].insert(0, "./data/force/");
 
-	files.push_back("./data/tdc_electrodes/cold_start.dat");
-	// files.push_back("./data/tdc_electrodes/cooling_down_from_high.dat");
-	// files.push_back("./data/tdc_electrodes/data_2015_07_30_09_44_43.dat");
-	files.push_back("./data/tdc_electrodes/data_2015_08_04_15_27_33.dat");
-	files.push_back("./data/tdc_electrodes/data_2015_08_04_16_15_04.dat");
-	// files.push_back("./data/tdc_electrodes/heating_up.dat");
-	files.push_back("./data/tdc_electrodes/high_temp.dat");
-			
+	std::vector<std::string> tdcFiles = nFileUtils::GetFilesInDirectory("./data/tdc_electrodes", ".dat");
+	for( std::size_t i = 0; i < tdcFiles.size(); ++i )
+	{
+		tdcFiles[i].insert(0, "./data/tdc_electrodes/");
+		files.push_back(tdcFiles[i]);
+	}
+
 	cBioTacForceCurve curve = ffTerms.TrainAgainstDataFiles(files);
 
 
 	// --- Test Fit --- //
-			
+
 	files.clear();
-	files.push_back("./data/tdc_electrodes/cold_start.dat");
-	// files.push_back("./data/tdc_electrodes/cooling_down_from_high.dat");
-	// files.push_back("./data/tdc_electrodes/data_2015_07_30_09_44_43.dat");
-	files.push_back("./data/tdc_electrodes/data_2015_08_04_15_27_33.dat");
-	files.push_back("./data/tdc_electrodes/data_2015_08_04_16_15_04.dat");
-	// files.push_back("./data/tdc_electrodes/heating_up.dat");
-	files.push_back("./data/tdc_electrodes/high_temp.dat");
-	files.push_back("./data/tdc_electrodes/unforced_already_warm.dat");
-	files.push_back("./data/test/data_2015_07_30_16_35_24.dat");
-	files.push_back("./data/test/data_2015_07_29_14_44_14.dat");
-	files.push_back("./data/test/data_2015_07_23_09_57_11.dat");
-	// files.push_back("./data/zero_force_01.dat");
-	// files.push_back("./data/zero_force_02.dat");
-	// files.push_back("./data/data_2015_07_23_09_50_24.dat");
-	// files.push_back("./data/data_2015_07_23_09_55_36.dat");
-	// files.push_back("./data/data_2015_07_23_09_55_48.dat");
-	ffTerms.TestAgainstDataFiles(files, curve);
+	files = nFileUtils::GetFilesInDirectory("./data/force", ".dat");
+	for( std::size_t i = 0; i < files.size(); ++i )
+		files[i].insert(0, "./data/force/");
+	// std::vector<std::string> dataFiles = nFileUtils::GetFilesInDirectory("./data/tdc_electrodes", ".dat");
+	// for( std::size_t i = 0; i < dataFiles.size(); ++i )
+	// {
+	// 	dataFiles[i].insert(0, "./data/tdc_electrodes/");
+	// 	files.push_back(dataFiles[i]);
+	// }
+
+	// ffTerms.TestAgainstDataFiles(files, curve);
+	std::ofstream csvFile("./error_temp.csv");
+	std::ofstream errRefFile("./error_ref.csv");
+	std::ofstream tempForceFile("./temp_force.csv");
+	std::ofstream tempErr100("./temp_err_100g.csv");
+	std::ofstream tempErr200("./temp_error_200g.csv");
+	std::ofstream tempErr300("./temp_error_300g.csv");
+	std::ofstream tempErr400("./temp_error_400g.csv");
+	for( std::size_t i = 0; i < files.size(); ++i )
+	{
+		cDataFileReader r(files[i]);
+		std::vector<biotac_tune_data> data = r.GetData();
+		double totalError = 0, avgTemp = 0, avgForce = 0;
+
+		for( std::size_t j = 0; j < data.size(); ++j )
+		{
+			std::vector<uint16_t> electrodes;
+			electrodes.push_back(data[j].mData.e1);
+			electrodes.push_back(data[j].mData.e2);
+			electrodes.push_back(data[j].mData.e3);
+			electrodes.push_back(data[j].mData.e4);
+			electrodes.push_back(data[j].mData.e5);
+			electrodes.push_back(data[j].mData.e6);
+			electrodes.push_back(data[j].mData.e7);
+			electrodes.push_back(data[j].mData.e8);
+			electrodes.push_back(data[j].mData.e9);
+			electrodes.push_back(data[j].mData.e10);
+			electrodes.push_back(data[j].mData.e11);
+			electrodes.push_back(data[j].mData.e12);
+			electrodes.push_back(data[j].mData.e13);
+			electrodes.push_back(data[j].mData.e14);
+			electrodes.push_back(data[j].mData.e15);
+			electrodes.push_back(data[j].mData.e16);
+			electrodes.push_back(data[j].mData.e17);
+			electrodes.push_back(data[j].mData.e18);
+			electrodes.push_back(data[j].mData.e19);
+			totalError += fabs(curve.GetForce_g( electrodes, data[j].mData.tdc, data[j].mData.pdc ) - data[j].mLoadCell_g);
+			avgTemp += data[j].mData.tdc;
+			avgForce += data[j].mLoadCell_g;
+		}
+
+		std::cout << files[i] << " - Total Error: " << totalError << ",\tAvg Error: " << totalError / data.size() << ",\tAvg T: " << avgTemp / data.size() << std::endl;
+		csvFile << avgTemp / data.size() << ", " << totalError / data.size() << std::endl;
+
+		errRefFile << avgForce / data.size() << ", " << totalError / data.size() << std::endl;
+
+		tempForceFile << avgTemp / data.size() << ", " << avgForce / data.size() << std::endl;
+
+		if( fabs((avgForce/data.size())-100) < 20 )
+		{
+			tempErr100 << avgTemp / data.size() << ", " << totalError / data.size() << std::endl;
+		}
+		else if( fabs((avgForce/data.size())-200) < 20 )
+		{
+			tempErr200 << avgTemp / data.size() << ", " << totalError / data.size() << std::endl;
+		}
+		else if( fabs((avgForce/data.size())-300) < 20 )
+		{
+			tempErr300 << avgTemp / data.size() << ", " << totalError / data.size() << std::endl;
+		}
+		else if( fabs((avgForce/data.size())-400) < 20 )
+		{
+			tempErr400 << avgTemp / data.size() << ", " << totalError / data.size() << std::endl;
+		}
+	}
 
 
 	// --- Write Trained Curve To File --- //
